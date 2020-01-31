aa14a4876664e91a171098fb6342ae57af54fb77
Peritios <49589640+Peritios@users.noreply.github.com>
2020-01-31 11:48:53 +0100

    Add Mech Dummy

---
+++ Assets/_GameAssets/Mecha/Move/MechDummy/SC_NetPlayerDummyP_P.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class SC_NetPlayerDummyP_P : NetworkBehaviour
+{
+
+    bool b_NotInLobby = false;
+    bool b_IsSpawn = false;
+
+    public GameObject MechDummy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isServer && !b_NotInLobby)
+            CheckScene();
+
+        if (isServer && b_NotInLobby && !b_IsSpawn)
+            SpawnMechDummy();
+
+    }
+
+    void CheckScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+            b_NotInLobby = true;
+    }
+
+    void SpawnMechDummy()
+    {
+        b_IsSpawn = true;
+        GameObject GO_MechDummy_Temp = (GameObject)Instantiate(MechDummy, MechDummy.transform.position, MechDummy.transform.rotation);
+        NetworkServer.Spawn(GO_MechDummy_Temp);
+    }
+
+}
