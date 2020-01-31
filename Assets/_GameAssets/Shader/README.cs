76a0b2879790d3c3fa4ac129d40ce515a3671e25
spetit42 <sonnypetit@hotmail.fr>
2020-01-31 13:45:55 +0100

    Terrain + Shader DepthMap
    Je push le prefab du terrain ainsi que le shader camera avec un petit README de comment l'appliquer a la camera

---
+++ Assets/_GameAssets/Shader/README.cs
@@ -0,0 +1,3 @@
+﻿//Pour appliquer le Shader a la Camera il faut mettre le script qui est dans le dossier shader sur la camera.
+//Drag and drop le material MAT_Zbuffer dans le script.
+//Reduire le far clipping de la camera a 300
