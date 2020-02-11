48f69dc85f745106f8b0c24781f83ccef22cc5c1
ebo-dev <58474274+ebo-sama@users.noreply.github.com>
2020-02-11 18:10:24 +0100

    Déso pas déso

--- Assets/_GameAssets/Mecha/Interactions_Cockpit/BreakDownMangement/Mng_BreakDown/Scripts/OP/SC_UI_Update_Switch.cs
+++ Assets/_GameAssets/Mecha/Interactions_Cockpit/BreakDownMangement/Mng_BreakDown/Scripts/OP/SC_UI_Update_Switch.cs
@@ -7,27 +7,19 @@ public class SC_UI_Update_Switch : MonoBehaviour
 {
     SC_SyncVar_BreakdownTest sc_syncvar;
     GameObject Mng_SyncVar = null;
-    /*
-    [SerializeField]
-    button bouton;
-    */
+
     Color curColor = new Color(255, 159, 0, 255);
     Color panneColor = new Color(255, 0, 0, 255);
 
     [SerializeField]
     Image stateInterrupteur;
 
+    [SerializeField]
+    Material curMat;
+    [SerializeField]
+    Material breakdownMat;
      public int index;
-    /*
-    enum button
-    {
-        inter1,
-        inter2,
-        inter3,
-        inter4,
-        inter5,
-    }*/
-    // Start is called before the first frame update
+
     void Start()
     {
         Mng_SyncVar = GameObject.FindGameObjectWithTag("Mng_SyncVar");
@@ -42,26 +34,17 @@ public class SC_UI_Update_Switch : MonoBehaviour
 
         if (sc_syncvar != null)
         {
-
-
            // Debug.Log("CLIENT : NB éléments ds struc : " + sc_syncvar.m_pows[index].power);
-            
-         
-
-
 
             if (sc_syncvar.SL_switches[index].isEnPanne)
             {
-                stateInterrupteur.color = panneColor;
+                stateInterrupteur.material = breakdownMat;
             }
             //NO PANNE
             else
             {
-                stateInterrupteur.color = curColor;
+                stateInterrupteur.material = curMat;
             }
-
-
-
         }
     }
 
