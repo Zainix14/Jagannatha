2ea5385e59f1d2826990023952027901c3284984
azymutt <fenrirans@gmail.com>
2020-02-13 17:47:56 +0100

    new pannescreens

---
+++ Assets/_GameAssets/Mecha/Interactions_Cockpit/Calibration/SC_SyncVar_calibr.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SC_SyncVar_calibr : NetworkBehaviour
+{
+    /// <summary>
+    /// //////////////////////////////////List struct des Sliders
+    /// </summary>
+    /// 
+    public int nbOfFloatButtons;
+
+    //nombre de sliders pour l'init
+    public SyncListFloat CalibrFloats = new SyncListFloat();
+
+
+
+    void Start()
+    {
+        if (isServer)
+        {
+            for (int i = 0; i < nbOfFloatButtons; i++)
+            {
+                CalibrFloats.Insert(i, 0f);
+            }
+        }
+    }
+
+
+
+
+
+}
---
+++ Assets/_GameAssets/Mecha/Interactions_Cockpit/Calibration/SC_slider_calibr.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SC_slider_calibr : MonoBehaviour {
+  private ViveGrip_ControllerHandler controller;
+  private float oldX;
+  private int VIBRATION_DURATION_IN_MILLISECONDS = 50;
+  private float MAX_VIBRATION_STRENGTH = 0.2f;
+  private float MAX_VIBRATION_DISTANCE = 0.03f;
+
+
+    private float _localX = 0;
+    private float _localY = 0;
+    private float _localZ = 0;
+    public bool _freezeAlongX = true;
+    public bool _freezeAlongY = false;
+    public bool _freezeAlongZ = true;
+
+    /// <summary>
+    /// Index du slider pour sa structList
+    /// </summary>
+    /// 
+
+    public int index;
+
+    public float desiredValue = 0;
+    public bool isEnPanne = false;
+    public float precision = 0.05f;
+
+    private GameObject Mng_SyncVar;
+    private Rigidbody sliderRigidbody;
+
+
+    private SC_SyncVar_calibr sc_syncvar_calibr;
+
+    void Start ()
+    {
+        oldX = transform.position.x;
+        sliderRigidbody = gameObject.GetComponent<Rigidbody>();
+        GetReferences();
+    }
+
+    void GetReferences()
+    {
+        
+        if (Mng_SyncVar == null)
+            Mng_SyncVar = GameObject.FindGameObjectWithTag("Mng_SyncVar");
+        if (Mng_SyncVar != null && sc_syncvar_calibr == null)
+            sc_syncvar_calibr = Mng_SyncVar.GetComponent<SC_SyncVar_calibr>();
+    }
+
+    void ViveGripGrabStart(ViveGrip_GripPoint gripPoint)
+    {
+    controller = gripPoint.controller;
+        sliderRigidbody.isKinematic = false;
+    }
+
+    void ViveGripGrabStop()
+    {
+        controller = null;
+        sliderRigidbody.isKinematic = true;
+    }
+
+	void Update () {
+        
+        //on traduit la position en position locale pour la freeze
+        _localX = transform.localPosition.x;
+        _localY = transform.localPosition.y;
+        _localZ = transform.localPosition.z;
+
+        if (_freezeAlongX) _localX = 0;
+        if (_freezeAlongY) _localY = 0;
+        if (_freezeAlongZ) _localZ = 0;
+        gameObject.transform.localPosition = new Vector3(_localX, _localY, _localZ);
+
+        
+     
+
+        float newX = gameObject.transform.localPosition.y;
+
+        //on envoie la valeur à la syncvar si celle ci a changé
+        if (newX != oldX) sendToSynchVar(Mathf.Round(gameObject.transform.localPosition.y*100)/100);
+
+
+
+        if (controller != null) {
+          float distance = Mathf.Min(Mathf.Abs(newX - oldX), MAX_VIBRATION_DISTANCE);
+          float vibrationStrength = (distance / MAX_VIBRATION_DISTANCE) * MAX_VIBRATION_STRENGTH;
+          controller.Vibrate(VIBRATION_DURATION_IN_MILLISECONDS, vibrationStrength);
+        }
+        oldX = newX;
+
+ 
+
+
+    }
+
+
+    
+
+    void sendToSynchVar (float value)
+    {
+
+        if (sc_syncvar_calibr == null)
+        {
+            GetReferences();
+        }
+        else
+        {
+
+            sc_syncvar_calibr.CalibrFloats[index] = value;
+
+        }          
+
+    }
+
+    
+}
