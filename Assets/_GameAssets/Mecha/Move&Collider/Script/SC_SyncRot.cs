6a94579c94b143c236bcc160fd136b90c410d561
Peritios <49589640+Peritios@users.noreply.github.com>
2020-02-13 11:59:40 +0100

    add script

---
+++ Assets/_GameAssets/Mecha/Move&Collider/Script/SC_SyncRot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_SyncRot : MonoBehaviour
+{
+
+    public Transform Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target != null)
+            this.transform.rotation = Target.rotation;
+    }
+}
