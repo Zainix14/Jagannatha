7e6dda46e0a76d6a521711d88ab6e04054f64238
ebo-dev <58474274+ebo-sama@users.noreply.github.com>
2020-02-13 17:28:25 +0100

    Grand Koa Coté OP

--- Assets/_GameAssets/Enemy/Elements/Flock/Scripts/SC_KoaSettingsOP.cs
+++ Assets/_GameAssets/Enemy/Elements/Flock/Scripts/SC_KoaSettingsOP.cs
@@ -6,6 +6,11 @@ public class SC_KoaSettingsOP : MonoBehaviour
 {
     Vector3 sensibility;
 
+    void update()
+    {
+        Debug.Log("Sensility : " + sensibility);
+    }
+
     public void SetSensibility(Vector3 sensibility)
     {
         this.sensibility = sensibility;
