dc8e4278ef4ff7615540c3510fb4e7dba2bc02f6
ebo-dev <58474274+ebo-sama@users.noreply.github.com>
2020-02-14 13:59:04 +0100

    New Script for raycast

---
+++ Assets/_GameAssets/OperatorSoftware/RayCastOnMap/SC_RaycastVirtual.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_RaycastVirtual : MonoBehaviour
+{
+    private RaycastHit hit;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //int layerMask = 1 << 9;
+        Debug.Log("Virtual raycast");
+        //Cast un ray à partir du casque
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("AHHHHAHAHAH CLIC");
+            Physics.Raycast(transform.position, Vector3.up, out hit, 500f);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+        }
+
+    }
+
+    /// <summary>
+    /// Renvois le raycasthit du cockpit
+    /// </summary>
+    /// <returns></returns>
+    public RaycastHit getRay()
+    {
+        return hit;
+    }
+
+}
