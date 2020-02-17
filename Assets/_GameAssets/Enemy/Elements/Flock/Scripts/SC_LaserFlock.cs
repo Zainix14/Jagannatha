7ca6a45547bb45ee980c18cb3da4dffd266ed201
Sonny <sonnypetit>
2020-02-17 18:30:40 +0100

    Asset Audio

--- Assets/_GameAssets/Enemy/Elements/Flock/Scripts/SC_LaserFlock.cs
+++ Assets/_GameAssets/Enemy/Elements/Flock/Scripts/SC_LaserFlock.cs
@@ -14,6 +14,7 @@ public class SC_LaserFlock : MonoBehaviour
             //https://www.youtube.com/watch?v=GBvfiCdk-jc&list=PLbsiLVHJCH9iHz_HDGirFtRUtKbdc9czK
             Sc_ScreenShake.Instance.ShakeIt(0.01f, 0.2f);
             //https://www.youtube.com/watch?v=nfWlot6h_JM
+            CustomSoundManager.Instance.PlaySound(gameObject, "SFX_p_Impact", false, 0.1f);
         }
     }
 
