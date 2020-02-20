19e20405237a40ab916dc5d53aa1973ede366b0f
Peritios <49589640+Peritios@users.noreply.github.com>
2020-02-20 18:12:56 +0100

    lasergun

---
+++ Assets/_GameAssets/Mecha/Weapon/LaserGun_3/Scripts/SC_BulletLaserGun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_BulletLaserGun : MonoBehaviour
+{
+
+    public Vector3Int sensitivity;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.layer == 26)
+            other.GetComponent<Boid>().HitBoid(sensitivity);
+
+        if (other.gameObject.layer == 25)
+            other.GetComponentInParent<SC_KoaCollider>().GetHit(sensitivity);
+
+    }
+
+}
