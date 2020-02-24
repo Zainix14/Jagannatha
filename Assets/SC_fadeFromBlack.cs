c075661a6541bdf73f87e273ed0c82ccc537f7ed
azymutt <fenrirans@gmail.com>
2020-02-24 12:16:42 +0100

    fadefromblack

---
+++ Assets/SC_fadeFromBlack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_fadeFromBlack : MonoBehaviour
+{
+    private void Awake()
+    {
+
+
+        SteamVR_Fade.Start(Color.black, 2f);
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
