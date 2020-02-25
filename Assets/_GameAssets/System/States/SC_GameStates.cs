e86898aebb2613a5e88fa61f46dc0b0b220f114f
azymutt <fenrirans@gmail.com>
2020-02-25 16:52:14 +0100

    debut instructions operateur retour du glow, reinversion main

--- Assets/_GameAssets/System/States/SC_GameStates.cs
+++ Assets/_GameAssets/System/States/SC_GameStates.cs
@@ -16,6 +16,18 @@ public class SC_GameStates : NetworkBehaviour
     public enum GameState {Lobby, Tutorial, Game, GameEnd }
     public GameState CurState;
 
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,11 +48,11 @@ public class SC_GameStates : NetworkBehaviour
                 break;
 
             case GameState.Tutorial:
-
+                
                 break;
 
             case GameState.Game:
-
+                SC_instruct_op_manager.Instance.Deactivate(0);
                 break;
 
             case GameState.GameEnd:
