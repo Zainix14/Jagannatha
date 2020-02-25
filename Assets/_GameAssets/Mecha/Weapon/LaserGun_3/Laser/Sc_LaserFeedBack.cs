b13f8e22b177805b43ca78362e063394d6d03158
Sonny <sonnypetit>
2020-02-25 11:16:54 +0100

    Revert "FixLaser"
    This reverts commit 67de173ec4356db935ac402390eabb3822801673.

--- Assets/_GameAssets/Mecha/Weapon/LaserGun_3/Laser/Sc_LaserFeedBack.cs
+++ Assets/_GameAssets/Mecha/Weapon/LaserGun_3/Laser/Sc_LaserFeedBack.cs
@@ -7,7 +7,6 @@ public class Sc_LaserFeedBack : MonoBehaviour
     public SC_WeaponLaserGun MainLaserScript;
     public GameObject FirePoint;
     public LineRenderer lr;
-    public LineRenderer lr2;
     [SerializeField]
     ParticleSystem Sparkle;
 
@@ -17,14 +16,8 @@ public class Sc_LaserFeedBack : MonoBehaviour
         if(!lr.enabled)
             lr.enabled = true;
 
-        lr.SetPosition(0, Vector3.zero);// FirePoint.transform.position);
-        lr2.SetPosition(0, Vector3.zero);// FirePoint.transform.position);
-
-        var targetPoint = lr.worldToLocalMatrix.MultiplyPoint(hit.point);
-        lr.SetPosition(1, targetPoint);// hit.point);
-        lr2.SetPosition(1, lr.GetPosition(1));
-
-        
+        lr.SetPosition(0, FirePoint.transform.position);
+        lr.SetPosition(1, hit.point);
 
         if (!Sparkle.isPlaying)
             Sparkle.Play();
