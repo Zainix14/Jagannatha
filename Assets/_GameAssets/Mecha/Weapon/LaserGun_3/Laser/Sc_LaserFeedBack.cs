67de173ec4356db935ac402390eabb3822801673
Sonny <sonnypetit>
2020-02-25 10:56:10 +0100

    FixLaser

--- Assets/_GameAssets/Mecha/Weapon/LaserGun_3/Laser/Sc_LaserFeedBack.cs
+++ Assets/_GameAssets/Mecha/Weapon/LaserGun_3/Laser/Sc_LaserFeedBack.cs
@@ -7,6 +7,7 @@ public class Sc_LaserFeedBack : MonoBehaviour
     public SC_WeaponLaserGun MainLaserScript;
     public GameObject FirePoint;
     public LineRenderer lr;
+    public LineRenderer lr2;
     [SerializeField]
     ParticleSystem Sparkle;
 
@@ -16,8 +17,14 @@ public class Sc_LaserFeedBack : MonoBehaviour
         if(!lr.enabled)
             lr.enabled = true;
 
-        lr.SetPosition(0, FirePoint.transform.position);
-        lr.SetPosition(1, hit.point);
+        lr.SetPosition(0, Vector3.zero);// FirePoint.transform.position);
+        lr2.SetPosition(0, Vector3.zero);// FirePoint.transform.position);
+
+        var targetPoint = lr.worldToLocalMatrix.MultiplyPoint(hit.point);
+        lr.SetPosition(1, targetPoint);// hit.point);
+        lr2.SetPosition(1, lr.GetPosition(1));
+
+        
 
         if (!Sparkle.isPlaying)
             Sparkle.Play();
