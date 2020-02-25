9c8b305974d05df1e88dc1268fae43342a02954b
Peritios <49589640+Peritios@users.noreply.github.com>
2020-02-25 11:16:26 +0100

    new laser

--- Assets/_GameAssets/Mecha/Weapon/LaserGun_3/Laser/Sc_LaserFeedBack.cs
+++ Assets/_GameAssets/Mecha/Weapon/LaserGun_3/Laser/Sc_LaserFeedBack.cs
@@ -7,6 +7,7 @@ public class Sc_LaserFeedBack : MonoBehaviour
     public SC_WeaponLaserGun MainLaserScript;
     public GameObject FirePoint;
     public LineRenderer lr;
+    public LineRenderer lr2;
     [SerializeField]
     ParticleSystem Sparkle;
 
@@ -15,9 +16,15 @@ public class Sc_LaserFeedBack : MonoBehaviour
 
         if(!lr.enabled)
             lr.enabled = true;
+        if (!lr2.enabled)
+            lr2.enabled = true;
 
-        lr.SetPosition(0, FirePoint.transform.position);
-        lr.SetPosition(1, hit.point);
+        var targetPoint = lr.worldToLocalMatrix.MultiplyPoint(hit.point);
+        lr.SetPosition(0, Vector3.zero);
+        lr.SetPosition(1, targetPoint);
+
+        lr2.SetPosition(0, Vector3.zero);
+        lr2.SetPosition(1, lr.GetPosition(1));
 
         if (!Sparkle.isPlaying)
             Sparkle.Play();
@@ -34,6 +41,8 @@ public class Sc_LaserFeedBack : MonoBehaviour
 
         if (lr.enabled)
             lr.enabled = false;
+        if (lr2.enabled)
+            lr2.enabled = false;
 
     }
 
