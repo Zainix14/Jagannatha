6a3bf02725416557663aea5b2bd38c51e88e7876
ebo-dev <58474274+ebo-sama@users.noreply.github.com>
2020-02-27 09:58:03 +0100

    Slider In Canvas Map Linkj to do

--- Assets/_GameAssets/OperatorSoftware/Display_2_MapInfos/StateKoa/SC_UI_Display_MapInfos_KoaState.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_2_MapInfos/StateKoa/SC_UI_Display_MapInfos_KoaState.cs
@@ -38,8 +38,8 @@ public class SC_UI_Display_MapInfos_KoaState : MonoBehaviour
     public float fKoaLife;
     public Vector3 koaSensibility;
     public Vector3 gunSensibility;
-    
 
+    public Slider sliderLifeKoa;
 
 
     void Awake()
@@ -98,7 +98,8 @@ public class SC_UI_Display_MapInfos_KoaState : MonoBehaviour
                 sensi[2].text = (koaSensibility.z + 1).ToString();
                 fKoaLife = (curKoaScriptKoaSettings.GetCurKoaLife() / curKoaScriptKoaSettings.GetMaxKoaLife()) * 100;
                 koaLife.text = fKoaLife.ToString();
-
+                Debug.Log(fKoaLife);
+                sliderLifeKoa.value = fKoaLife;
                 gunSensibility = new Vector3(sc_syncvar.CalibrInts[0], sc_syncvar.CalibrInts[1], sc_syncvar.CalibrInts[2]);
             
                 optiWeapon.text = GetOptiPerCent().ToString();
