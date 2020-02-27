9b14083e446ead666d98041c8b725dfc5a2ddcd8
Zainix <49592220+Zainix@users.noreply.github.com>
2020-02-27 11:03:58 +0100

    Oper Good

--- Assets/_GameAssets/OperatorSoftware/Display_2_MapInfos/StateKoa/SC_UI_Display_MapInfos_KoaState.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_2_MapInfos/StateKoa/SC_UI_Display_MapInfos_KoaState.cs
@@ -97,12 +97,12 @@ public class SC_UI_Display_MapInfos_KoaState : MonoBehaviour
                 sensi[1].text = (koaSensibility.y + 1).ToString();
                 sensi[2].text = (koaSensibility.z + 1).ToString();
                 fKoaLife = (curKoaScriptKoaSettings.GetCurKoaLife() / curKoaScriptKoaSettings.GetMaxKoaLife()) * 100;
-                koaLife.text = fKoaLife.ToString() + "%";
+                koaLife.text = fKoaLife.ToString();
                 //Debug.Log(fKoaLife);
                 sliderLifeKoa.value = fKoaLife;
                 gunSensibility = new Vector3(sc_syncvar.CalibrInts[0], sc_syncvar.CalibrInts[1], sc_syncvar.CalibrInts[2]);
             
-                optiWeapon.text = GetOptiPerCent().ToString();
+                optiWeapon.text = GetOptiPerCent().ToString() + "%";
 
                 type.text = "Type " + curKoaScriptKoaSettings.GetKoaID().ToString();
             }
