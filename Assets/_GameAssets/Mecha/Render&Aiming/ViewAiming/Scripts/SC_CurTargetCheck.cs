3651b973215744882f19e33808a668f13bfbddea
Peritios <49589640+Peritios@users.noreply.github.com>
2020-03-05 12:11:26 +0100

    readd layer

---
+++ Assets/_GameAssets/Mecha/Render&Aiming/ViewAiming/Scripts/SC_CurTargetCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_CurTargetCheck : MonoBehaviour
+{
+
+    public bool b_OnKoa = false;
+    public Collider Curtarget = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 10 && !b_OnKoa)
+        {
+            b_OnKoa = true;
+            Curtarget = other;
+        }        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 10 && b_OnKoa)
+        {
+            b_OnKoa = false;
+        }
+    }
+
+}
