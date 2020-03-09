5c3ea7981186959b0e81576a31299384667ab2c7
Peritios <49589640+Peritios@users.noreply.github.com>
2020-03-09 18:08:02 +0100

    joystick en cours

---
+++ Assets/_GameAssets/Mecha/Interactions_Cockpit/JoyStick/SC_JoystickRenderMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_JoystickRenderMove : MonoBehaviour
+{
+
+    float f_ImpulseX;
+    float f_TransImpulseZ;
+    float f_TorqueImpulseZ;
+
+    [Range(0,90)]
+    public float f_MaxRotH;
+    [Range(0, 90)]
+    public float f_MaxRotV;
+    [Range(0, 90)]
+    public float f_MaxRotT;
+
+    float f_CurRotH;
+    float f_CurRotV;
+    float f_CurRotT;
+
+    [SerializeField]
+    Transform TargetTRS;
+
+    void Update()
+    {
+        GetRotation();
+        ApplyRot();
+    }
+
+    void GetRotation()
+    {
+
+        f_TransImpulseZ = Input.GetAxis("Horizontal");
+        f_ImpulseX = Input.GetAxis("Vertical");
+        f_TorqueImpulseZ = Input.GetAxis("Torque");
+
+        f_CurRotH = (Mathf.Abs(f_TransImpulseZ) * f_MaxRotH / 1) * Mathf.Sign(f_TransImpulseZ);
+        f_CurRotT = (Mathf.Abs(f_TorqueImpulseZ) * f_MaxRotT / 1) * Mathf.Sign(f_TorqueImpulseZ);
+        f_CurRotV = (Mathf.Abs(f_ImpulseX) * f_MaxRotV / 1) * Mathf.Sign(f_ImpulseX);
+
+        Debug.Log((-90 + f_CurRotV) + " - " + f_MaxRotT + " - " + f_CurRotH);
+
+    }
+
+    void ApplyRot()
+    {
+        //TargetTRS.transform.Rotate(-90 + f_CurRotV, f_MaxRotT, f_CurRotH, Space.Self); 
+        Quaternion targetRot = new Quaternion();
+        targetRot.eulerAngles = new Vector3(-90 - f_CurRotH, f_CurRotT, f_CurRotV);
+        //TargetTRS.localRotation = targetRot;
+
+    }
+
+}
