9db9bb5213cd921354c8a1109a3413c46623a3df
Peritios <49589640+Peritios@users.noreply.github.com>
2020-03-11 15:49:49 +0100

    rescale snap hitbox realtime

--- Assets/_GameAssets/Enemy/Elements/Flock/Scripts/SC_SnapCollider.cs
+++ Assets/_GameAssets/Enemy/Elements/Flock/Scripts/SC_SnapCollider.cs
@@ -8,6 +8,31 @@ public class SC_SnapCollider : MonoBehaviour
     GameObject Mng_CheckList;
     Camera MechCam;
 
+    [SerializeField]
+    CapsuleCollider Collider;
+    [SerializeField]
+    float DistFromMech;
+
+    [Header("Height Parameters")]
+    [SerializeField]
+    float MinHeight = 1;
+    [SerializeField]
+    float MaxHeight = 50;
+    [SerializeField]
+    float MinDistHeight = 1;
+    [SerializeField]
+    float MaxDistHeight = 126;
+
+    [Header("Radius Parameters")]
+    [SerializeField]
+    float MinRadius = 2;
+    [SerializeField]
+    float MaxRadius = 8;
+    [SerializeField]
+    float MinDistRadius = 20;
+    [SerializeField]
+    float MaxDistRadius = 126;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +46,11 @@ public class SC_SnapCollider : MonoBehaviour
             GetReferences();
 
         if (MechCam != null)
+        {
             LookAtMech();
+            UpdateDimension();
+        }   
+
     }
 
     void GetReferences()
@@ -35,6 +64,53 @@ public class SC_SnapCollider : MonoBehaviour
     void LookAtMech()
     {
         this.transform.LookAt(MechCam.transform);
+    }
+
+    void UpdateDimension()
+    {
+
+        Vector3 Dist = this.transform.position - MechCam.transform.position;
+        DistFromMech = Dist.magnitude;
+
+        if(DistFromMech < MinDistHeight)
+            Collider.height = MinHeight;
+        else if(DistFromMech > MaxDistHeight)
+            Collider.height = MaxHeight;
+        else
+        {
+
+            float HeightRange = MaxHeight - MinHeight;
+            float DistRange = MaxDistHeight - MinDistHeight;
+            float CurDistRange = DistFromMech - MinDistHeight;
+
+            float CurDistRatio = CurDistRange * 100 / DistRange;
+            float CurHeightRatio = CurDistRatio / 100 * HeightRange;
+            float CurHeight = CurHeightRatio + MinHeight;
+
+            Collider.height = CurHeight;
+
+        }
+
+        
+        if (DistFromMech < MinDistRadius)
+            Collider.radius = MinRadius;
+        else if (DistFromMech > MaxDistRadius)
+            Collider.radius = MaxRadius;
+        else
+        {
+
+            float RadiusRange = MaxRadius - MinRadius;
+            float DistRange = MaxDistRadius - MinDistRadius;
+            float CurDistRange = DistFromMech - MinDistRadius;
+
+            float CurDistRatio = CurDistRange * 100 / DistRange;
+            float CurRadiusRatio = CurDistRatio / 100 * RadiusRange;
+            float CurRadius = CurRadiusRatio + MinRadius;
+
+            Collider.radius = CurRadius;
+        }
+        
+
     }
 
 }
