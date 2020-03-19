d421962f57f7d93895ce80967e0cd6cd61000c3f
azymutt <azymutt@users.noreply.github.com>
2020-03-19 16:46:27 +0100

    Fix fleche vision

--- Assets/_GameAssets/OperatorSoftware/Display_1_Map/SC_UI_vision_arrow.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_1_Map/SC_UI_vision_arrow.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class SC_UI_vision_arrow : MonoBehaviour
 {
 
+    private GameObject mechdummy;
+
     Vector3 rot = new Vector3();
 
     // Start is called before the first frame update
     void Start()
     {
+        mechdummy = GameObject.FindGameObjectWithTag("mech_dummy");
         rot.x = 90;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rot.y = SC_SyncVar_vision_arrow.Instance.rotCasque+90;
+
+        rot.y = SC_SyncVar_vision_arrow.Instance.rotCasque+90 + mechdummy.transform.eulerAngles.y;
 
         transform.eulerAngles = rot;
     }
