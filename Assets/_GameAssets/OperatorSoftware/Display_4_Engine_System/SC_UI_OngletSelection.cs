c6c00b2c154040a6a86aa276823588283d88d024
ebo-dev <58474274+ebo-sama@users.noreply.github.com>
2020-03-20 13:40:07 +0100

    Operator -> Display Up -> Onglet : setActive devient transf.pos

--- Assets/_GameAssets/OperatorSoftware/Display_4_Engine_System/SC_UI_OngletSelection.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_4_Engine_System/SC_UI_OngletSelection.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class SC_UI_OngletSelection : MonoBehaviour, IF_clicableAction
 {
     public int index;
-    public SC_UI_OngletContainer scriptWithChild;
+    public SC_UI_OngletContainer ongletContainer;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,24 +22,39 @@ public class SC_UI_OngletSelection : MonoBehaviour, IF_clicableAction
 
     public void Action()
     {
-        if(this.index != null)
+        //if(this.index != null)
+        //{
+        //    for(int i= 0; i < scriptWithChild.child.Length; i++)
+        //    {
+        //        if(i == index)
+        //        {
+        //            scriptWithChild.child[i].SetActive(true);
+        //            scriptWithChild.checkActive();
+        //        }
+        //        else
+        //        {
+        //            scriptWithChild.child[i].SetActive(false);
+        //        }
+
+        //    }
+
+        //}
+
+        Debug.Log("Clic sur onglet" + index);
+        for (int i = 0; i < ongletContainer.child.Length; i++)
         {
-            for(int i= 0; i < scriptWithChild.child.Length; i++)
+            if (i == index)
             {
-                if(i == index)
-                {
-                    scriptWithChild.child[i].SetActive(true);
-                    scriptWithChild.checkActive();
-                }
-                else
-                {
-                    scriptWithChild.child[i].SetActive(false);
-                }
-                    
+                ongletContainer.curIndex = i;
+                ongletContainer.checkActive();
+                ongletContainer.child[i].transform.localPosition = new Vector3(0, 0, 0);
             }
-            
+            else
+            {
+                ongletContainer.child[i].transform.localPosition = new Vector3(0, 0, 400);
+            }
         }
 
-       
+            
     }
 }
