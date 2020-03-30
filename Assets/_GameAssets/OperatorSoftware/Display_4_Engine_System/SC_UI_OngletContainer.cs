46806a47f0fd97d84a065b12f8689b80764fcc1e
ebo-dev <58474274+ebo-sama@users.noreply.github.com>
2020-03-30 16:26:06 +0200

    Op -> Onglet Prepa

--- Assets/_GameAssets/OperatorSoftware/Display_4_Engine_System/SC_UI_OngletContainer.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_4_Engine_System/SC_UI_OngletContainer.cs
@@ -19,13 +19,17 @@ public class SC_UI_OngletContainer : MonoBehaviour
 
     [SerializeField]
     GameObject[] onglet;
-    Vector3 ongletActifPos;
+    [SerializeField]
+    Vector3[] hubInOngletPos;
     public Transform particleFB;
     //public RectTransform particleRect;
     bool toPlace = false;
+    bool hubOn = true;
     // Start is called before the first frame update
     public int curIndex;
 
+    [SerializeField]
+    float speedAnimOnglet;
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -43,37 +47,66 @@ public class SC_UI_OngletContainer : MonoBehaviour
     {
         particleFB = particleFB.GetComponent<RectTransform>();
         curIndex = 0;
-        //checkActive();
-        preshotPos();
+        checkActive();
+        //preshotPos();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (toPlace)
-        {
-            particleFB.localPosition = Vector3.Lerp(ongletActifPos, particleFB.localPosition, Time.deltaTime*50f);
-            if (particleFB.transform.localPosition == ongletActifPos && ongletActifPos != Vector3.zero)
-            {
-                toPlace = false;
-            }
+        //if (toPlace)
+        //{
+        //    particleFB.localPosition = Vector3.Lerp(ongletActifPos, particleFB.localPosition, Time.deltaTime*50f);
+        //    if (particleFB.transform.localPosition == ongletActifPos && ongletActifPos != Vector3.zero)
+        //    {
+        //        toPlace = false;
+        //    }
 
+        //}
+        if (!hubOn)
+        {
+            child[curIndex].transform.localPosition = Vector3.Lerp(child[curIndex].transform.localPosition, Vector3.zero, Time.deltaTime * speedAnimOnglet);
+            child[curIndex].transform.localScale = Vector3.Lerp(child[curIndex].transform.localScale, Vector3.one, Time.deltaTime * speedAnimOnglet);
+            child[0].transform.localPosition = hubInOngletPos[curIndex - 1];
+            child[0].transform.localScale = Vector3.zero;
+            
+        }
+        else
+        {
+            child[0].transform.localPosition = Vector3.Lerp(child[0].transform.localPosition, Vector3.zero, Time.deltaTime * speedAnimOnglet);
+            child[0].transform.localScale = Vector3.Lerp(child[0].transform.localScale, Vector3.one, Time.deltaTime * speedAnimOnglet);
         }
          
     }
 
     public void checkActive()
     {
-        for(int i =0; i< child.Length;i++)
+        if (curIndex == 0)
         {
-            if(i == curIndex)
+            hubOn = true;
+            Debug.Log("Hub");
+            for (int i = 0; i < onglet.Length; i++)
             {
-                ongletActifPos = onglet[i].transform.localPosition;
-                toPlace = true;
+                //child[i].transform.position = onglet[i+1].transform.localPosition;
+                child[i+1].transform.localPosition = onglet[i].transform.localPosition;
+                child[i + 1].transform.localScale = Vector3.zero;
             }
-            
         }
+        else
+        {
+            hubOn = false;
+            for (int i = 0; i < child.Length; i++)
+            {
+
+                if (i == curIndex)
+                {
+                    //ongletActifPos = onglet[i - 1].transform.localPosition;
+                    
+                    toPlace = true;
+                }
+            }
+        }
+        
     }
     //Only for the start
     void preshotPos()
