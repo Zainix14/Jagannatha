7fc645af70442e057f23b80e481466ce0535e0d2
ebo-dev <58474274+ebo-sama@users.noreply.github.com>
2020-04-02 09:45:28 +0200

    All System connected

--- Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_Movement_MechState.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_Movement_MechState.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class SC_Movement_MechState : MonoBehaviour
 {
+    #region Singleton
+
+    private static SC_Movement_MechState _instance;
+    public static SC_Movement_MechState Instance { get { return _instance; } }
+
+    #endregion
+
+    SC_SyncVar_MovementSystem sc_syncvar;
+    GameObject Mng_SyncVar = null;
+
+    [SerializeField]
+    SC_UI_SystmShield _SystmShield;
+
+    void Awake()
+    {
+
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Mng_SyncVar = GameObject.FindGameObjectWithTag("Mng_SyncVar");
+        GetReferences();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (sc_syncvar == null || Mng_SyncVar == null)
+            GetReferences();
+
+        if (sc_syncvar != null)
+        {
+            _SystmShield.simpleValue = sc_syncvar.f_MovementLife;
+        }
+    }
+
+    void GetReferences()
+    {
+        if (Mng_SyncVar == null)
+            Mng_SyncVar = GameObject.FindGameObjectWithTag("Mng_SyncVar");
+        if (Mng_SyncVar != null && sc_syncvar == null)
+            sc_syncvar = Mng_SyncVar.GetComponent<SC_SyncVar_MovementSystem>();
     }
 }
