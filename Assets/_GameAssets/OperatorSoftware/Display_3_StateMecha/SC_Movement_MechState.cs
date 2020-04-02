37af4ea73397978d56953d2e18f3d768516b463c
Peritios <49589640+Peritios@users.noreply.github.com>
2020-04-02 10:11:02 +0200

    appel de singleton c mieux

--- Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_Movement_MechState.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_Movement_MechState.cs
@@ -11,9 +11,6 @@ public class SC_Movement_MechState : MonoBehaviour
 
     #endregion
 
-    SC_SyncVar_MovementSystem sc_syncvar;
-    GameObject Mng_SyncVar = null;
-
     [SerializeField]
     SC_UI_SystmShield _SystmShield;
 
@@ -30,30 +27,18 @@ public class SC_Movement_MechState : MonoBehaviour
         }
 
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        Mng_SyncVar = GameObject.FindGameObjectWithTag("Mng_SyncVar");
-        GetReferences();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (sc_syncvar == null || Mng_SyncVar == null)
-            GetReferences();
 
-        if (sc_syncvar != null)
-        {
-            _SystmShield.simpleValue = sc_syncvar.f_MovementLife;
-        }
+        UpdateValue();
+
     }
 
-    void GetReferences()
+    void UpdateValue()
     {
-        if (Mng_SyncVar == null)
-            Mng_SyncVar = GameObject.FindGameObjectWithTag("Mng_SyncVar");
-        if (Mng_SyncVar != null && sc_syncvar == null)
-            sc_syncvar = Mng_SyncVar.GetComponent<SC_SyncVar_MovementSystem>();
+        _SystmShield.simpleValue = SC_SyncVar_MovementSystem.Instance.f_MovementLife;
     }
+
 }
