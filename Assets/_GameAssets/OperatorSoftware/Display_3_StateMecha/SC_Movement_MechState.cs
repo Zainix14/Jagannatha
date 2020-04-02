c02307aeab0139a0ecfe09f28ea7992f779513b2
ebo-dev <58474274+ebo-sama@users.noreply.github.com>
2020-04-02 15:03:47 +0200

    Weapon OP Panne

--- Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_Movement_MechState.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_Movement_MechState.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SC_Movement_MechState : MonoBehaviour
 {
@@ -18,6 +19,15 @@ public class SC_Movement_MechState : MonoBehaviour
     SC_Movement_Direction dirRight;
     [SerializeField]
     SC_Movement_Direction dirLeft;
+
+    [SerializeField]
+    Image[] arcR;
+    [SerializeField]
+    Image[] arcL;
+    [SerializeField]
+    Color32 validColor;
+    [SerializeField]
+    Color32 breakdownColor;
 
 
     public enum SystemState { Disconnected, Connected, Initialize, Launched }
@@ -37,35 +47,23 @@ public class SC_Movement_MechState : MonoBehaviour
 
     }
 
+    private void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-
         UpdateValue();
-
+        updateDirection();
+        updateBrokenDirection();
     }
 
     void UpdateValue()
     {
         _SystmShield.simpleValue = SC_SyncVar_MovementSystem.Instance.f_MovementLife;
-        if(SC_SyncVar_MovementSystem.Instance.CurDir != SC_JoystickMove.Dir.None)
-        {
-            if (SC_SyncVar_MovementSystem.Instance.CurDir == SC_JoystickMove.Dir.Right)
-            {
-                dirRight.speedRotateBase = dirRight.speedRotateUsed;
-                dirLeft.speedRotateBase = dirLeft.speedRotateInit;
-            }
-            else if (SC_SyncVar_MovementSystem.Instance.CurDir == SC_JoystickMove.Dir.Left)
-            {
-                dirLeft.speedRotateBase = dirLeft.speedRotateUsed;
-                dirRight.speedRotateBase = dirRight.speedRotateInit;
-            }
-        }
-        else
-        {
-            dirRight.speedRotateBase = dirRight.speedRotateInit;
-            dirLeft.speedRotateBase = dirLeft.speedRotateInit;
-        }
+        
     }
 
     void CheckState()
@@ -93,5 +91,71 @@ public class SC_Movement_MechState : MonoBehaviour
         }  
 
     }
+    
+    void updateDirection()
+    {
+        if (SC_SyncVar_MovementSystem.Instance.CurDir != SC_JoystickMove.Dir.None)
+        {
+            if (SC_SyncVar_MovementSystem.Instance.CurDir == SC_JoystickMove.Dir.Right)
+            {
+                dirRight.speedRotateBase = dirRight.speedRotateUsed;
+                dirLeft.speedRotateBase = dirLeft.speedRotateInit;
+            }
+            else if (SC_SyncVar_MovementSystem.Instance.CurDir == SC_JoystickMove.Dir.Left)
+            {
+                dirLeft.speedRotateBase = dirLeft.speedRotateUsed;
+                dirRight.speedRotateBase = dirRight.speedRotateInit;
+            }
+        }
+        else
+        {
+            dirRight.speedRotateBase = dirRight.speedRotateInit;
+            dirLeft.speedRotateBase = dirLeft.speedRotateInit;
+        }
+    }
 
+    void updateBrokenDirection()
+    {
+        if(SC_SyncVar_MovementSystem.Instance.CurBrokenDir == SC_JoystickMove.Dir.Left)
+        {
+            int nbBreakdown = SC_SyncVar_MovementSystem.Instance.n_BreakDownLvl;
+            if(nbBreakdown !=0)
+            {
+                for (int i = 0; i < nbBreakdown; i++)
+                {
+                    arcL[i].color = breakdownColor;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < arcL.Length; i++)
+                {
+                    arcL[i].color = validColor;
+                }
+            }
+            
+        }
+        else if (SC_SyncVar_MovementSystem.Instance.CurBrokenDir == SC_JoystickMove.Dir.Right)
+        {
+            int nbBreakdown = SC_SyncVar_MovementSystem.Instance.n_BreakDownLvl;
+            if (nbBreakdown != 0)
+            {
+                Debug.Log("Panne");
+                for (int i = 0; i < nbBreakdown; i++)
+                {
+                    arcR[i].color = breakdownColor;
+                }
+            }
+            else
+            {
+                
+                for (int i = 0; i < arcL.Length; i++)
+                {
+                    arcR[i].color = validColor;
+                }
+            }
+
+        }
+
+    }
 }
