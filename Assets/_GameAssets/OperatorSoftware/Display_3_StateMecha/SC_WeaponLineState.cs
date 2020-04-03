ba55c2400882e339ceace0019f5dca06c71b0a8f
Peritios <49589640+Peritios@users.noreply.github.com>
2020-04-03 18:48:44 +0200

    add line

---
+++ Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_WeaponLineState.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_WeaponLineState : MonoBehaviour
+{
+
+    [SerializeField]
+    int n_CurveSize = 300;
+
+    [SerializeField]
+    RectTransform Container;
+
+    [SerializeField]
+    AnimationCurve FunctionalState;
+    [SerializeField]
+    AnimationCurve BreakdownStateLvl01;
+    [SerializeField]
+    AnimationCurve BreakdownStateLvl02;
+
+    AnimationCurve CurCurve;
+
+    LineRenderer line;
+    Color32 curColor;
+
+    float f_MaxX = 376;
+    float f_MaxY = 170;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = this.gameObject.GetComponent<LineRenderer>();
+        curColor = new Color32();
+        CurCurve = FunctionalState;
+        f_MaxX = Container.rect.width;
+        f_MaxY = Container.rect.height;
+    }
+
+    public void UpdateLine()
+    {
+
+        switch (SC_SyncVar_WeaponSystem.Instance.f_CurNbOfBd)
+        {
+
+            case 0:
+                CurCurve = FunctionalState;
+                break;
+
+            case 1:
+                CurCurve = BreakdownStateLvl01;
+                break;
+
+            case 2:
+                CurCurve = BreakdownStateLvl02;
+                break;
+
+        }
+
+        line.positionCount = 300; //Configuration du nombre 
+        for (int i = 0; i < line.positionCount; i++)
+        {
+            float x = (i * (f_MaxX / line.positionCount));
+            float y = CurCurve.Evaluate(x) * f_MaxY;
+            line.SetPosition(i, new Vector3(y, 0f, x));
+        }
+
+    }
+
+}
