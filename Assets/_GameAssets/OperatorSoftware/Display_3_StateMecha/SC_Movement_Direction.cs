4a34d0c6ac789fba464494ccb6cb900a2ac2a70f
Peritios <49589640+Peritios@users.noreply.github.com>
2020-04-03 15:58:07 +0200

    Fix Rotation

--- Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_Movement_Direction.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_Movement_Direction.cs
@@ -13,72 +13,68 @@ public class SC_Movement_Direction : MonoBehaviour
 
     public float CurRotationSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-
-        //this.transform.Rotate(Vector3.up * CurRotationSpeed *  Time.deltaTime, Space.World);
         Rotate();
-
     }
 
     void Rotate()
     {
 
-        if(!b_IsBreak)
+        if (!SC_SyncVar_MovementSystem.Instance.b_BreakEngine)
         {
-            if(b_InUse)
-                this.transform.Rotate(Vector3.up * speedRotateUsed * Time.deltaTime, Space.World);
+
+            if (!b_IsBreak)
+            {
+                if (b_InUse)
+                    this.transform.Rotate(Vector3.up * speedRotateUsed * Time.deltaTime, Space.World);
+                else
+                    this.transform.Rotate(Vector3.up * speedRotateInit * Time.deltaTime, Space.World);
+            }
+
             else
-                this.transform.Rotate(Vector3.up * speedRotateInit * Time.deltaTime, Space.World);
-        }
-
-        else
-        {
-
-            switch (SC_SyncVar_MovementSystem.Instance.n_BreakDownLvl)
             {
 
-                case 0:
-                    if (b_InUse)
-                        this.transform.Rotate(Vector3.up * speedRotateUsed * Time.deltaTime, Space.World);
-                    else
-                        this.transform.Rotate(Vector3.up * speedRotateInit * Time.deltaTime, Space.World);
-                    break;
+                switch (SC_SyncVar_MovementSystem.Instance.n_BreakDownLvl)
+                {
 
-                case 1:
-                    if (b_InUse)
-                        this.transform.Rotate(Vector3.up * ( speedRotateUsed / 2 ) * Time.deltaTime, Space.World);
-                    else
-                        this.transform.Rotate(Vector3.up * speedRotateInit * Time.deltaTime, Space.World);
-                    break;
+                    case 0:
+                        if (b_InUse)
+                            this.transform.Rotate(Vector3.up * speedRotateUsed * Time.deltaTime, Space.World);
+                        else
+                            this.transform.Rotate(Vector3.up * speedRotateInit * Time.deltaTime, Space.World);
+                        break;
 
-                case 2:
-                    if (b_InUse)
-                    {
-                        int n_Corofactor = 1;
-                        if (SC_SyncVar_MovementSystem.Instance.CoroDir == SC_JoystickMove.Dir.None || SC_SyncVar_MovementSystem.Instance.CoroDir == SC_JoystickMove.Dir.Off)
-                            n_Corofactor = 0;
-                        this.transform.Rotate(Vector3.up * (speedRotateUsed / 2) * n_Corofactor * Time.deltaTime, Space.World);
-                    }      
-                    else
-                        this.transform.Rotate(Vector3.up * speedRotateInit * Time.deltaTime, Space.World);
-                    break;
+                    case 1:
+                        if (b_InUse)
+                            this.transform.Rotate(Vector3.up * (speedRotateUsed / 2) * Time.deltaTime, Space.World);
+                        else
+                            this.transform.Rotate(Vector3.up * speedRotateInit * Time.deltaTime, Space.World);
+                        break;
 
-                case 3:
-                    this.transform.Rotate(Vector3.up * 0 * Time.deltaTime, Space.World);
-                    break;
+                    case 2:
+                        if (b_InUse)
+                        {
+                            int n_Corofactor = 1;
+                            if (SC_SyncVar_MovementSystem.Instance.CoroDir == SC_JoystickMove.Dir.None || SC_SyncVar_MovementSystem.Instance.CoroDir == SC_JoystickMove.Dir.Off)
+                                n_Corofactor = 0;
+                            this.transform.Rotate(Vector3.up * (speedRotateUsed / 2) * n_Corofactor * Time.deltaTime, Space.World);
+                        }
+                        else
+                            this.transform.Rotate(Vector3.up * speedRotateInit * Time.deltaTime, Space.World);
+                        break;
+
+                    case 3:
+                        this.transform.Rotate(Vector3.up * 0 * Time.deltaTime, Space.World);
+                        break;
+
+                }
 
             }
 
         }
-
+        
     }
 
 }
