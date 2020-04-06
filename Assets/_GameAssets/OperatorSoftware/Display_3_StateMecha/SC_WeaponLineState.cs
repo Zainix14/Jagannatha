1539960c667b851c17259e03dffae9c54b574116
Peritios <49589640+Peritios@users.noreply.github.com>
2020-04-06 16:18:28 +0200

    TestCoro

--- Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_WeaponLineState.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_3_StateMecha/SC_WeaponLineState.cs
@@ -5,36 +5,40 @@ using UnityEngine;
 public class SC_WeaponLineState : MonoBehaviour
 {
 
-    [SerializeField]
-    int n_CurveSize = 300;
-
+    [Header("References")]
     [SerializeField]
     RectTransform Container;
+    LineRenderer line;
 
+    [Header("Curves Parameters")]
     [SerializeField]
     AnimationCurve FunctionalState;
     [SerializeField]
     AnimationCurve BreakdownStateLvl01;
     [SerializeField]
     AnimationCurve BreakdownStateLvl02;
+    [SerializeField]
+    int n_CurveSize = 300;
+    float f_MaxX = 376;
+    float f_MaxY = 170;
+    Color32 curColor;
 
+    [Header("Curves Infos")]
     [SerializeField]
     AnimationCurve CurCurve;
 
-    LineRenderer line;
-    Color32 curColor;
-
-    float f_MaxX = 376;
-    float f_MaxY = 170;
+    Coroutine CurCoro;
 
     // Start is called before the first frame update
     void Start()
     {
         line = this.gameObject.GetComponent<LineRenderer>();
-        curColor = new Color32();
         CurCurve = FunctionalState;
         f_MaxX = Container.rect.width;
         f_MaxY = Container.rect.height;
+        line.positionCount = n_CurveSize;
+
+        curColor = new Color32();
     }
 
     public void UpdateLine()
@@ -57,15 +61,56 @@ public class SC_WeaponLineState : MonoBehaviour
 
         }
 
-        line.positionCount = n_CurveSize; //Configuration du nombre 
-        float n_TempCount = line.positionCount;
+        if (CurCoro != null)
+            StopCoroutine(CurCoro);
 
-        for (int i = 0; i < n_TempCount; i++)
+        //CurCoro = StartCoroutine(DrawLine());
+        CurCoro = StartCoroutine(DrawLineII(1));
+
+    }
+
+    IEnumerator DrawLine()
+    {
+
+        if (line.positionCount != n_CurveSize)
+            line.positionCount = n_CurveSize; //Configuration du nombre 
+
+        for (int i = 0; i < n_CurveSize; i++)
         {
-            float x = (i * (f_MaxX / n_TempCount));
-            float y = CurCurve.Evaluate((1 / n_TempCount) * i) * f_MaxY;
-            //float y = CurCurve.Evaluate(x);
+            float x = (i * (f_MaxX / n_CurveSize));
+            float y = CurCurve.Evaluate((1 / n_CurveSize) * i) * f_MaxY;
             line.SetPosition(i, new Vector3(x, y, 0f));
+            yield return 0;
+        }
+
+    }
+
+    IEnumerator DrawLineII(float Duration)
+    {
+
+        float time = 0;
+        float rate = 1 / Duration;
+        float f_TimePerUnit = Duration / n_CurveSize;
+
+        if (line.positionCount != n_CurveSize)
+            line.positionCount = n_CurveSize; //Configuration du nombre 
+
+        for (int i = 0; i < n_CurveSize; i++)
+        {
+
+            if(time < f_TimePerUnit*i)
+            {
+                time += Time.deltaTime * rate;
+                float x = (i * (f_MaxX / n_CurveSize));
+                float y = CurCurve.Evaluate((1 / n_CurveSize) * i) * f_MaxY;
+                line.SetPosition(i, new Vector3(x, y, 0f));
+            }
+
+            else
+            {
+                yield return 0;
+            }
+
         }
 
     }
