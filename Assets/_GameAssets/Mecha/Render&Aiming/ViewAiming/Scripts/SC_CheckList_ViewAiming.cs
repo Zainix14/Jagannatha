c3ba7aacc400c7de50251ddc9d2875cb610a2e05
Peritios <49589640+Peritios@users.noreply.github.com>
2020-04-08 17:25:35 +0200

    checklist are now all singleton

--- Assets/_GameAssets/Mecha/Render&Aiming/ViewAiming/Scripts/SC_CheckList_ViewAiming.cs
+++ Assets/_GameAssets/Mecha/Render&Aiming/ViewAiming/Scripts/SC_CheckList_ViewAiming.cs
@@ -5,8 +5,28 @@ using UnityEngine;
 public class SC_CheckList_ViewAiming : MonoBehaviour
 {
 
+    #region Singleton
+
+    private static SC_CheckList_ViewAiming _instance;
+    public static SC_CheckList_ViewAiming Instance { get { return _instance; } }
+
+    #endregion
+
+    [Header("Aim References")]
     public GameObject Screens = null;
     public GameObject Target = null;
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
 
     public GameObject GetTarget()
     {
--- Assets/_GameAssets/Mecha/Weapon/System/SC_CheckList_Weapons.cs
+++ Assets/_GameAssets/Mecha/Weapon/System/SC_CheckList_Weapons.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class SC_CheckList_Weapons : MonoBehaviour
 {
 
+    #region Singleton
+
+    private static SC_CheckList_Weapons _instance;
+    public static SC_CheckList_Weapons Instance { get { return _instance; } }
+
+    #endregion
+
+    [Header("Aim References")]
     public GameObject TargetHand = null;
     public GameObject AimIndicator = null;
 
+    [Header("Managers")]
     public GameObject Mng_Weapons = null;
+
+    [Header("Weapons")]
+    public GameObject LaserGun = null;
+
+    [Header("Weapons (No More Used)")]
     public GameObject MiniGun = null;
     public GameObject FlameThrower = null;
-    public GameObject Shrapnel = null;
-    public GameObject LaserGun = null;
+    public GameObject Shrapnel = null;  
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
 
     public GameObject GetMngWeapons()
     {
