bbe501b6aef32be148c7bd366ec59339daeb11ed
Zainix14 <49592220+Zainix14@users.noreply.github.com>
2020-04-09 20:27:15 +0200

    Feedback hub panne

--- Assets/_GameAssets/OperatorSoftware/Display_4_Engine_System/SC_UI_OngletSelection.cs
+++ Assets/_GameAssets/OperatorSoftware/Display_4_Engine_System/SC_UI_OngletSelection.cs
@@ -11,15 +11,16 @@ public class SC_UI_OngletSelection : MonoBehaviour, IF_clicableAction, IF_Hover
     bool isBreakdown;
 
     [SerializeField]
-    Animator warningNotif;
-    [SerializeField]
-    Image img_warningNotif;
-
-    [SerializeField]
     GameObject animated;
     Animator animator;
 
+    [SerializeField]
+    Image[] img_ToBreakDown;
+   
+    Material[] wireSafe;
 
+    [SerializeField]
+    Material wireBreakdown;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +28,13 @@ public class SC_UI_OngletSelection : MonoBehaviour, IF_clicableAction, IF_Hover
         ongletContainer = SC_UI_OngletContainer.Instance;
         if(animated != null)
         animator = animated.GetComponent<Animator>();
+
+        wireSafe = new Material[img_ToBreakDown.Length];
+
+        for(int i = 0; i<wireSafe.Length; i++)
+        {
+            wireSafe[i] = img_ToBreakDown[i].material;
+        }
 
     }
 
@@ -72,14 +80,80 @@ public class SC_UI_OngletSelection : MonoBehaviour, IF_clicableAction, IF_Hover
     {
         if(state)
         {
-            this.warningNotif.SetBool("b_OnNotif", true);
-            
+            //this.warningNotif.SetBool("b_OnNotif", true);
+            StartCoroutine(RedWireCoro());
         }
         else
         {
-            this.warningNotif.SetBool("b_OnNotif", false);
+            //this.warningNotif.SetBool("b_OnNotif", false);
+            
+            EndCoroutine();
         }
     }
+
+    void EndCoroutine()
+    {
+        StopAllCoroutines();
+        for (int i = 0; i<img_ToBreakDown.Length; i++)
+        {
+            img_ToBreakDown[i].material = wireSafe[i];
+            img_ToBreakDown[i].color = Color.white;
+        }
+
+    }
+    IEnumerator RedWireCoro()
+    {
+        float animTime = 0.5f;
+        float maxOpacity = 1;
+        float minOpacity = 0f;
+        float ratePerSec = maxOpacity - minOpacity / animTime;
+        float curOpacity;
+        bool Add = true;
+        float t = 0;
+
+        for (int i = 0; i < img_ToBreakDown.Length; i++)
+        {
+            img_ToBreakDown[i].material = wireBreakdown;
+        }
+
+        Vector4 ColorTampon = Color.white;
+        curOpacity = minOpacity;
+
+        while (true)
+        {
+            if(t < animTime)
+            {
+                t += Time.deltaTime;
+                if (Add)
+                {
+
+                    if (curOpacity < maxOpacity)
+                         curOpacity = Mathf.Lerp(curOpacity,maxOpacity, ratePerSec * Time.deltaTime);
+                }
+                else
+                {
+
+                    if (curOpacity > minOpacity)
+                        curOpacity = Mathf.Lerp(curOpacity, minOpacity, ratePerSec * Time.deltaTime);
+
+                }
+
+                for (int i = 0; i < img_ToBreakDown.Length; i++)
+                {
+                    img_ToBreakDown[i].color = new Vector4(ColorTampon.x, ColorTampon.y, ColorTampon.z, curOpacity);
+                }
+                
+            }
+            else
+            {
+                Add = !Add;
+                t = 0;
+            }
+            yield return 0;
+        }
+        
+    }
+
 
 
     void IsHover()
