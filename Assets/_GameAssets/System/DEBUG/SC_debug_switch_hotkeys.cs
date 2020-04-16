74100c3f5c8d8efaf50449c60c9c4befbecd7ea4
ebo-dev <58474274+ebo-sama@users.noreply.github.com>
2020-04-16 16:47:54 +0200

    Debug.

--- Assets/_GameAssets/System/DEBUG/SC_debug_switch_hotkeys.cs
+++ Assets/_GameAssets/System/DEBUG/SC_debug_switch_hotkeys.cs
@@ -12,6 +12,8 @@ public class SC_debug_switch_hotkeys : MonoBehaviour
     public SC_SwitchChangeColor Button4;
     public SC_SwitchChangeColor Button5;
 
+    [SerializeField]
+    ViveGripExample_Switch[] _Switch;
 
     // Start is called before the first frame update
     void Start()
@@ -26,22 +28,27 @@ public class SC_debug_switch_hotkeys : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Button1.LightOnOFF();
+           _Switch[0].Flip();
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             Button2.LightOnOFF();
+           _Switch[1].Flip();
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             Button3.LightOnOFF();
+            _Switch[2].Flip();
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             Button4.LightOnOFF();
+           _Switch[3].Flip();
         }
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             Button5.LightOnOFF();
+            _Switch[4].Flip();
         }
 
     }
