b1c72019a713dd1e430f8e228cc8cc881415aa67
Peritios <49589640+Peritios@users.noreply.github.com>
2020-04-23 15:36:22 +0200

    new scripts

---
+++ Assets/_GameAssets/Mecha/Interactions_Cockpit/BreakDownMangement/Movement_BreakDown/SC_ShowSequence_OP.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_ShowSequence_OP : MonoBehaviour
+{
+
+    #region Singleton
+
+    private static SC_ShowSequence_OP _instance;
+    public static SC_ShowSequence_OP Instance { get { return _instance; } }
+
+    #endregion
+
+    [Header("References")]
+    [SerializeField]
+    GameObject SeqPart01;
+    [SerializeField]
+    GameObject SeqPart02;
+    [SerializeField]
+    GameObject SeqPart03;
+
+    [Header("Debug References")]
+    [SerializeField]
+    bool b_UseDebugContent = false;
+    [SerializeField]
+    GameObject DebugCnt01;
+    [SerializeField]
+    GameObject DebugCnt02;
+    [SerializeField]
+    GameObject DebugCnt03;
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public void DisplaySequence()
+    {
+
+    }
+
+}
