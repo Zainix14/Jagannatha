7d7582ce7fd45fb960df2ffdfac8166095fda435
Zainix14 <49592220+Zainix14@users.noreply.github.com>
2020-05-01 19:25:40 +0200

    j'ai faim, je veux une pizza

---
+++ Assets/_GameAssets/OperatorSoftware/Global_Script/SC_UI_BlinkBreakdown.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_UI_BlinkBreakdown : MonoBehaviour
+{
+    #region Singleton
+
+    private static SC_UI_BlinkBreakdown _instance;
+    public static SC_UI_BlinkBreakdown Instance { get { return _instance; } }
+    #endregion
+
+    SC_UI_WireBlink wireBlink;
+
+    int nbIndex;
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        wireBlink = GetComponent<SC_UI_WireBlink>();
+        nbIndex = wireBlink.img_ToBreakDown.Length;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            StartTotalBreakDownBlink();
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            StopTotalBreakDownBlink();
+        }
+    }
+    // Start is called before the first frame update
+    public void StartTotalBreakDownBlink()
+    {
+        for(int i =0; i < nbIndex; i++)
+        {
+            wireBlink.SetBreakDown(i, true,0.5f,1,1);
+        }
+    }
+
+    public void StopTotalBreakDownBlink()
+    {
+        for (int i = 0; i < nbIndex; i++)
+        {
+            wireBlink.SetBreakDown(i, false);
+        }
+    }
+}
